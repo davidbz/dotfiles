69072758792f571559ee61b460c35e64854789bc
David Ben Zakai <davidbenzakai@gmail.com>
2018-03-28 14:42:09 +0300

    Adding xinitrc

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIRECTORY=$(cd `dirname $0` && pwd)
 
-cp -vr ${SCRIPT_DIRECTORY}/.config ~/
+cp -vfr ${SCRIPT_DIRECTORY}/dotfiles/* ~/
 
 echo "Refreshing pacman keys"
 sudo pacman-key --refresh-keys
@@ -41,13 +41,12 @@ sudo pacman -S --needed i3 rofi \
                     wget \
                     nodejs \
                     jq \
-                    coreutils \
-                    yaourt
+                    coreutils
 
 echo "Cleaning up cache"
 sudo pacman -Sc
 
-yaourt --noconfirm parcellite
-yaourt --noconfirm deepin-screenshot
-yaourt --noconfirm ttf-ms-fonts
-yaourt --noconfirm sublime
+#yaourt --noconfirm parcellite
+#yaourt --noconfirm deepin-screenshot
+#yaourt --noconfirm ttf-ms-fonts
+#yaourt --noconfirm sublime
