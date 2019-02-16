bcad38e2d109fac1653c95a3a17afe67091007c7
David Ben Zakai <david.benzakai@gmail.com>
2019-02-16 22:54:35 +0200

    Some changes

--- README.md
+++ README.md
@@ -4,20 +4,13 @@ My setup. Using Arch and i3-gaps.
 
 ## Getting Started
 
-* [Arch installation guide](https://wiki.archlinux.org/index.php/installation_guide)
-* [i3](https://wiki.archlinux.org/index.php/i3)
+* Read [Arch installation guide](https://wiki.archlinux.org/index.php/installation_guide)
+* Read [i3 docs](https://wiki.archlinux.org/index.php/i3)
+* Read [st docs](https://wiki.archlinux.org/index.php/st)
 
 ## Deployment
 
-Install the following using Pacman:
-
-```
-xorg-xinit
-xorg
-i3-gaps
-```
-
-Install the following using AUR packge manager:
+After running install.sh install the following using AUR packge manager:
 ```
 st-git
 ttf-ms-fonts
--- install.sh
+++ install.sh
@@ -1,56 +1,46 @@
 #!/bin/bash
 
-SCRIPT_DIRECTORY=$(cd `dirname $0` && pwd)
-
 echo "Refreshing pacman keys"
-sudo pacman-key --refresh-keys
+pacman-key --refresh-keys
 
 echo "Refreshing mirrors"
-sudo pacman -Sy
+pacman -Sy
 
-echo "Installing packges"
-sudo pacman -S --needed zsh \
-                    git \
-                    unzip \
-                    unrar \
-                    tar \
-                    gzip \
-                    wireshark-gtk \
-                    vim \
-                    htop \
-                    strace \
-                    make \
-                    gcc \
-                    autoconf \
-                    gdb \
-                    cmake \
-                    docker \
-                    chromium \
-                    openssh \
-                    rsync \
-                    sed \
-                    tree \
-                    wget \
-                    nodejs \
-                    jq \
-                    coreutils \
-		    go
+pacman -S xorg-xinit \
+          xorg \
+          i3-gaps \
+          git \
+          tar \
+          wireshark-gtk \
+          vim \
+          htop \
+          strace \
+          make \
+          gcc \
+          autoconf \
+          gdb \
+          cmake \
+          docker \
+          chromium \
+          openssh \
+          wget \
+          nodejs \
+          coreutils \
+          go \
+          compton
 
 echo "Cleaning up cache"
-sudo pacman -Sc
+pacman -Sc
 
-pushd /tmp/
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
-popd
+if pacman -Q yay > /dev/null 2>&1; then
+     pushd /tmp/
+     git clone https://aur.archlinux.org/yay.git
+     cd yay
+     makepkg -si
+     popd
+     rm -rf /tmp/yay
+fi
 
 if pacman -Q zsh > /dev/null 2>&1; then
 	chsh -s $(whereis zsh | awk '{print $2}')
 fi
-
-ZSH_RC_ADDITIONALS="[ -f ~/.aliases ] && source ~/.aliases"
-if ! grep -q "${ZSH_RC_ADDITIONALS}" ~/.zshrc; then
-  echo "${ZSH_RC_ADDITIONALS}" >> ~/.zshrc
-fi
-
